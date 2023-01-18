dee4974942202ec6a9b6689cdf88edb9338be0a3
sanar4 <anarsalamov@gmail.com>
2023-01-18 10:14:37 +0400

    React Router Installation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { CssBaseline } from "@mui/material";
-// import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <CssBaseline />
-      <h1>Initial Setup - Filmpire</h1>
+      <main>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="movie" element={<h1>Movie Information</h1>} />
+          <Route path="actors" element={<h1>Actors</h1>} />
+          <Route path="movies" element={<h1>Movies</h1>} />
+          <Route path="profile" element={<h1>Profile</h1>} />
+        </Routes>
+      </main>
     </div>
   );
 };
